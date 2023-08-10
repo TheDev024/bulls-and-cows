fc686e4a8ad6deec3d99cbb6d6b255a9eec21a95
TheDev024 <thedeveloper024@gmail.com>
2023-08-10 15:45:01 +0400

    secret generation modified

--- src/main/kotlin/org/td024/bullsandcows/Generator.kt
+++ src/main/kotlin/org/td024/bullsandcows/Generator.kt
@@ -5,8 +5,8 @@ class Generator {
     companion object {
 
         fun generateSecret(size: Int): String {
-            val secretCharArray = System.nanoTime().toString().toSet().toCharArray()
-            return if (secretCharArray.size >= size) String(secretCharArray).take(size)
+            val secretCharArray = CharArray(size) { ('0'..'9').random() }.toSet().toCharArray()
+            return if (secretCharArray.size >= size) String(secretCharArray).takeLast(size)
             else generateSecret(size)
         }
     }
